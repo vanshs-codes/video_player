2c0468688247eb8beab6f0246076a8c86952d464
Vansh Singla <vansh54sn@gmail.com>
2025-06-04 19:35:31 +0530

    added some utils

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
-connectDB();
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`app listening on port: ${process.env.PORT || 8000}`);
+    })
+})
+.catch((error) => {
+    console.log("db connection failed: ", error);
+});
